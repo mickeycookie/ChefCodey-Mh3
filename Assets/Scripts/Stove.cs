050d42e897864f42473e81ec90b0a7c7a0df7f94
mickeycookie <mikaelaham@icloud.com>
2025-11-08 13:05:14 -0500

    i will fix possibly next week

--- Assets/Scripts/Stove.cs
+++ Assets/Scripts/Stove.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class Stove : MonoBehaviour
 {
+    [Header("Prefabs")]
     public GameObject toast;
     public GameObject friedEgg;
 
+    [Header("Inventory")]
     public string cookedFood = "";
+
+    [Header("Particles")]
+    public ParticleSystem smoke;
+    public ParticleSystem complete;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,14 +21,18 @@ public class Stove : MonoBehaviour
 
     public void ToastBread()
     {
+        smoke.Play();
         toast.SetActive(true);
         cookedFood = "toast";
+        Invoke("CompleteCooking", 6f); 
     }
 
     public void FryEgg()
     {
+        smoke.Play();
         friedEgg.SetActive(true);
         cookedFood = "friedEgg";
+        Invoke("CompleteCooking", 8f);
     }
 
     public void CleanStove()
@@ -30,6 +40,12 @@ public class Stove : MonoBehaviour
         toast.SetActive(false);
         friedEgg.SetActive(false);
         cookedFood = "";
+    }
+
+    private void CompleteCooking()
+    {
+        smoke.Stop();
+        complete.Play();
     }
     // Update is called once per frame
     void Update()
